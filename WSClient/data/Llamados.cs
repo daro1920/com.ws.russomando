f8bc4383c812104a0b53f5653b1991ceb0b5948c
Rubens Barrios <daro1920@gmail.com>
2016-08-15 21:59:13 -0300

    WS cliente
    Update

--- WSClient/data/Llamados.cs
+++ WSClient/data/Llamados.cs
@@ -44,10 +44,7 @@ namespace WSClient.models
                     llamadosList.Add(row);
 
                 }
-
-
-                //dataGridView1.DataSource = YourResultSet;
-                //dataGridView1.DataBindingComplete();
+                
 
                 yourConnectionHandler.Close();
             }
@@ -56,35 +53,16 @@ namespace WSClient.models
 
         public void setLlamados(Int32 llaid)
         {
+            
 
-            List<DataRow> llamadosList = new List<DataRow>();
-
-            // creo datatable para los llamados
-            DataTable llamados = new DataTable();
-
-            OleDbConnection yourConnectionHandler = new OleDbConnection(@"Provider=VFPOLEDB.1;Data Source=C:\Work\FreelanceProjects\RoussoMando\dbf");
-
-            // Open the connection, and if open successfully, you can try to query it
-            yourConnectionHandler.Open();
-
-            if (yourConnectionHandler.State == ConnectionState.Open)
+            using (OleDbConnection con = new OleDbConnection(@"Provider=VFPOLEDB.1;Data Source=C:\Work\FreelanceProjects\RoussoMando\dbf"))
+            using (OleDbCommand command = con.CreateCommand())
             {
-                string sqlLlamados = "UPDATE llamados SET EMPCOD = 0 WHERE LLAID = ?";  // dbf table name
-
-                OleDbCommand queryLlamados = new OleDbCommand(sqlLlamados, yourConnectionHandler);
-                queryLlamados.Parameters.Add("LLAID", OleDbType.Char, llaid, "LLAID");
-
-
-                OleDbDataAdapter DALlamados = new OleDbDataAdapter(queryLlamados);
-
-                DALlamados.UpdateCommand = queryLlamados;
-
-
-                yourConnectionHandler.Close();
+                command.CommandText = "update llamados set EMPCOD = 155 where LLAID = "+ llaid.ToString();
+                con.Open();
+                command.ExecuteNonQuery();
+                con.Close();
             }
-
-
-       
 
         }
 
